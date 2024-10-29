a25cedcacd1f14ba179e0a8b35fcaad05b8cc9de
yuzu <s.yuzu441@gmail.com>
2024-10-29 21:52:49 +0900

    feat(bookmarklet): add md-copy-link

---
+++ src/cmd/md_copy_link.ts
@@ -0,0 +1,8 @@
+(async (): Promise<void> => {
+  const title = document.title;
+  const url = location.href;
+
+  const mdLink = `[${title}](${url})`;
+
+  await navigator.clipboard.writeText(mdLink);
+})();
